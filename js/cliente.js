e9b6ee42dd709c641517e1dc8df563de7d63e68a
Anderson <eusouanderson@outlook.com>
2023-03-23 16:16:18 -0300

    "updat"

---
+++ js/cliente.js
@@ -0,0 +1,17 @@
+function login(){
+    let login = window.document.getElementById('name')
+    let res = window.document.getElementById('res')
+    let password = window.document.getElementById('password')
+    if (login.value.length == 0 && password.value.length == 0){
+        window.alert('[ERRO] Digite o login fornecido !')
+        res.innerHTML = "Erro ao logar!"
+    }if (login.value == 'admin' && password.value == '123'){
+        window.alert('Logado com sucesso')
+        res.innerHTML = "Logando..!"
+    }else{
+        window.alert('[Erro] Tente novamente! ')
+        res.innerHTML = "Login invalido..!"
+    }
+    } 
+   
+    login.value.innerHTML = 'Ola'
