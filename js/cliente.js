872dd6445467f497f92c00ccdf44ea0b03be9199
Anderson <eusouanderson@outlook.com>
2023-03-24 17:51:34 -0300

    "update"

--- js/cliente.js
+++ js/cliente.js
@@ -1,17 +1,26 @@
+
+let container = window.document.getElementById('container1');
+container.style.display = "none"
 function login(){
-    let login = window.document.getElementById('name')
-    let res = window.document.getElementById('res')
-    let password = window.document.getElementById('password')
+    let login = window.document.getElementById('name');
+    let res = window.document.getElementById('res');
+    let password = window.document.getElementById('password');
+    let button = window.document.getElementById('action-btn1');
+    let cliente = window.document.getElementById('cliente')
     if (login.value.length == 0 && password.value.length == 0){
-        window.alert('[ERRO] Digite o login fornecido !')
+        window.alert('[ERRO] Digite o login fornecido !');
         res.innerHTML = "Erro ao logar!"
     }if (login.value == 'admin' && password.value == '123'){
-        window.alert('Logado com sucesso')
-        res.innerHTML = "Logando..!"
+        window.alert('Logado com sucesso');
+        res.innerHTML = "Logado..!"
+        container.style.display ="block"
+        cliente.style.display = "none"
     }else{
-        window.alert('[Erro] Tente novamente! ')
+        window.alert('[Erro] Tente novamente! ');
         res.innerHTML = "Login invalido..!"
     }
-    } 
-   
+};
+
+
+
 
